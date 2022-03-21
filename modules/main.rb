a50cd4eafc7e2ccc66f778e244fbfb1b74b0e390
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-03-21 19:34:02 +0600

    Add main branch

---
+++ modules/main.rb
@@ -0,0 +1,31 @@
+class Main
+  def start
+    puts 'Welcome to the App'
+    puts 'Choose an option below:'
+
+    loop do
+      option = list_options
+      break if option == 10
+
+      puts 'List books' if option == 1
+    end
+  end
+
+  def list_options
+    puts '1. List all books'
+    puts '2. List all music albums'
+    puts '3. List all games'
+    puts '4. List all genres'
+    puts '5. List all labels'
+    puts '6. List all authors'
+    puts '7. Add a book'
+    puts '8. Add a music album'
+    puts '9. Add a game'
+    puts '10. Exit'
+
+    gets.chomp.to_i
+  end
+end
+
+main = Main.new
+main.start
