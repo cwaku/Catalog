ea9aa47073f39e93a1d20c5bb6e7a5dde9caf1bd
Ricky Mormor <rickymormor1@gmail.com>
2022-03-22 12:18:08 +0000

    Read book title

--- modules/book.rb
+++ modules/book.rb
@@ -1,7 +1,7 @@
 require_relative './item'
 
 class Book < Item
-  attr_accessor :publisher, :cover_state
+  attr_accessor :publisher, :cover_state, :title
 
   def initialize(title, publisher, cover_state, publish_date)
     super(publish_date)
